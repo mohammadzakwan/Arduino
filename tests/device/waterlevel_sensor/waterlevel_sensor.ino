39edb7ee1f54ae7b6e1ccee6e3a01b7392e75cef
Muhammad Zakwan <mohammadzakwan@gmail.com>
2019-01-10 11:50:10 +0500

    Create waterlevel_sensor.ino

---
+++ tests/device/waterlevel_sensor/waterlevel_sensor.ino
@@ -0,0 +1,125 @@
+
+
+// I am trying to put a webserver on my Wemos D1 mini pro.
+//I am using this example for the Arduino code: https://github.com/wemos/Arduino_D1/blob/master/doc/esp8266wifi/server-examples.rst
+
+#include <ESP8266WiFi.h>
+
+const char* ssid = "SmartSwitch";
+const char* password = "admin@123";
+const int pingPin = D4;
+const int TrigPin = D3;
+long duration, inches, cm;
+ 
+
+WiFiServer server(80);
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  server.begin();
+  Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
+}
+
+
+// prepare a web page to be send to a client (web browser)
+String prepareHtmlPage()
+{
+  String htmlPage =
+ //    String("Refresh: 1\r\n") +
+      
+      String("HTTP/1.1 200 OK \r") +
+//      "Distance:  " + String(Water_Level()) +
+//            "HTTP/1.1 200 OK\r\n") +
+//            "Content-Type: text/html\r\n" +
+//            "Connection: close\r\n" +  // the connection will be closed after completion of the response
+//"\r\n" +
+            "Refresh: 1 \r" +  // refresh the page automatically every 5 sec
+//            "\r\n" +
+//            "<!DOCTYPE HTML>" +
+//            "<html>" +
+            "Distance:  " + String(Water_Level()) +
+//            "</html>" +
+            "\r\n";
+  return htmlPage;
+}
+
+
+//String Water_Level()
+long Water_Level()
+{
+
+String Level= String(inches);
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  
+  pinMode(TrigPin, INPUT);
+  duration = pulseIn(TrigPin, HIGH);
+
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+//return Level;
+return inches;
+}
+long microsecondsToInches(long microseconds) {
+
+  return microseconds / 74 / 2;
+  
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  
+  return microseconds / 29 / 2;
+  
+}
+
+
+void loop()
+{
+  WiFiClient client = server.available();
+  client.println(Water_Level());
+//  delay(1000);
+  client.println(10);
+  delay(100);
+//  Serial.println(prepareHtmlPage());
+//Serial.println(Water_Level());
+//  // wait for a client (web browser) to connect
+//  if (client)
+//  {
+//    Serial.println("\n[Client connected]");
+////    Serial.println(distance);
+//    while (client.connected())
+//    {
+//      // read line by line what the client (web browser) is requesting
+//     if (client.available())  
+//{  
+//        String line = client.readStringUntil('\r');  
+//        Serial.print(line);  
+//        // wait for end of client's request, that is marked with an empty line  
+//        if (line.length() == 1 && line[0] == '\n')  
+//        {  
+//          client.println(prepareHtmlPage());  
+//          break;  
+//        }  
+//          client.println(prepareHtmlPage()); 
+//      } 
+//    }
+//  }
+}
